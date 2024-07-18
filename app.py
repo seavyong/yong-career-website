8e5a5734800ccbd4ef8354c450e72caadaf09e38
seavyong <134144836+seavyong@users.noreply.github.com>
2024-07-18 14:07:07 +0000

    addd dynamic daa and api route

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
-
+Jobs = [
+    {
+        'id' : 1,
+        'tittle': 'Data Analyst',
+        'location': 'Bengaluru, India',
+         'salary': '1000$'
+    },
+    {
+        'id' : 2,
+        'tittle': 'Data Science',
+        'location': 'Phnom Penh, Cambodia',
+        'salary': '1000$'
+    }
+]
 @app.route("/")
 def hello_world():
-    return render_template('home.html')
+    return render_template('home.html', jobs = Jobs, company_name = 'Yong')
+
+@app.route("/api/jobs")
+def list_jobs():
+    return jsonify(Jobs)
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug = True)
